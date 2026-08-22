0c81a980f82deb0eed1b2b808dbaa183b917b797
Juan Carlos <159584867+JuanCMath@users.noreply.github.com>
2026-08-22 08:55:00 -0400

    chore(frontend): add environment template and lint config

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ["@typescript-eslint", "react-hooks", "react-refresh"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended"
+  ],
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  settings: {
+    react: { version: "18.3" }
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
+  },
+  ignorePatterns: ["dist", "node_modules"]
+};
